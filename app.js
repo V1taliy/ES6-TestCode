27d7f50d478076a54fb3a8233376d76345787e1e
Vitaliy <biggone24@gmail.com>
2018-03-28 01:44:30 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/*const li = document.createElement('li');
+li.className = 'collection-item';
+
+li.id = 'new-item';
+
+li.setAttribute('title', 'New Item');
+
+
+li.appendChild(document.createTextNode('Hello World'));
+document.querySelector('ul').appendChild(li);
+
+console.log(li);
+
+
+const newHeading = document.createElement('h2');
+newHeading.id = 'task-titile';
+newHeading.appendChild(document.createTextNode('Tasl List'));
+const oldHeading = document.getElementById('task-title');
+const cardAction = document.querySelector('.card-action');
+
+cardAction.replaceChild(newHeading, oldHeading);
+//REMOVE ELEMENT
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+lis[0].remove();
+list.removeChild(lis[3]);
+
+console.log(newHeading)
+*/
+
